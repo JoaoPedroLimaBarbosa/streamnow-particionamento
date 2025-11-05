692b8c8f7b5fe249e3b660bfc227da20882fc704
JoaoPedroLimaBarbosa <joaopedrolimabarbosa007@gmail.com>
2025-11-05 08:38:37 -0300

    Update 01_create_tables.sql

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -1,1 +1,17 @@
+-- Tabela de usuários particionada
+CREATE TABLE usuarios (
+    id_usuario SERIAL,
+    nome VARCHAR(100),
+    pais VARCHAR(50),
+    data_cadastro DATE NOT NULL,
+    plano VARCHAR(20)
+) PARTITION BY RANGE (data_cadastro);
 
+-- Tabela de reproduções particionada
+CREATE TABLE reproducoes (
+    id_reproducao SERIAL,
+    id_usuario INT,
+    data_reproducao DATE NOT NULL,
+    duracao_segundos INT,
+    categoria VARCHAR(50)
+) PARTITION BY RANGE (data_reproducao);
