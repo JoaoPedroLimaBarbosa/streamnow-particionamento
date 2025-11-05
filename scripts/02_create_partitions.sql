77c86dc0c6ce2b36071bcab2e9a74a273cce8866
JoaoPedroLimaBarbosa <joaopedrolimabarbosa007@gmail.com>
2025-11-05 08:39:09 -0300

    Create partition tables for usuarios and reproducoes
    Added partition tables for usuarios and reproducoes for the years 2023 and 2024.

--- scripts/02_create_partitions.sql
+++ scripts/02_create_partitions.sql
@@ -1,1 +1,14 @@
+-- Partições usuarios
+CREATE TABLE usuarios_2023 PARTITION OF usuarios
+FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+CREATE TABLE usuarios_2024 PARTITION OF usuarios
+FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
+
+-- Partições reproduções
+CREATE TABLE reproducoes_2023 PARTITION OF reproducoes
+FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+CREATE TABLE reproducoes_2024 PARTITION OF reproducoes
+FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
 
